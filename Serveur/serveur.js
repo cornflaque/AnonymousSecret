50d60055d70f537dc544726bb59abf8883c1506c
user.name <antheau@yahoo.fr>
2019-03-11 20:26:45 +0100

    envoi message

--- Serveur/serveur.js
+++ Serveur/serveur.js
@@ -17,8 +17,29 @@ var created = false;
 var users = new Array;
 var nbusers = 0;
 
+// Fonction pour naviguer entre les pages
+function createJoueur(name, score) {
+
+	var id = users.length + 1;
+	var joueur = {
+		id : id,
+		name : name,
+		score : score
+	}
+	return joueur;
+}
+
 io.sockets.on('connection',function(socket) {
 
+	// Faire une tempo pour la page de résultat intermédiaire
+	socket.on('boolean', function (boolean) {
+
+      console.log('Joueur a voter: ' + boolean);
+
+    });
+	if(created && users.length < nbusers){
+		socket.emit('joingame');
+	}
 	console.log('Nouveau utilisateur');
 
 	socket.on("newuser", function(namejoueur, nbjoueurs){
@@ -26,31 +47,33 @@ io.sockets.on('connection',function(socket) {
 		// Cas 1 : premier nbjoueur
 		// Cas 2 : joueur qui rejoint
 		// Cas 3 : joueur en trop
-	console.log('Name : '+namejoueur+'   nbjoueurs : '+nbjoueurs);
+		console.log('Name : '+namejoueur+'   nbjoueurs : '+nbjoueurs);
 		// First user
 		if(!created && nbjoueurs != null){
 			console.log("premier joueur")
 			nbusers = nbjoueurs;
-			users.push(namejoueur);
-			socket.emit('waitingothers');
+
+			// On initialise le score à 0
+			users.push(createJoueur(namejoueur, 0));
+			console.log(users);			socket.emit('waitingothers');
 			created = true;
 			socket.broadcast.emit('joingame');
 		}
 		else{
 
-		// Joining user
-		if(created && users.length < nbusers){
-			console.log("Joining user")
-			users.push(namejoueur);
-			if(users.length == nbusers){
-				console.log("beginninggame")
-				socket.broadcast.emit('beginningame');
-			}
-			else{
-				console.log("on attend les autres");
-				socket.emit('waitingothers');
+			// Joining user
+			if(created && users.length < nbusers){
+				console.log("Joining user")
+				users.push(createJoueur(namejoueur, 0));
+				console.log(users);			if(users.length == nbusers){
+					console.log("beginninggame")
+					socket.broadcast.emit('beginningame');
+				}
+				else{
+					console.log("on attend les autres");
+					socket.emit('waitingothers');
+				}
 			}
 		}
-}
 	})
 });
