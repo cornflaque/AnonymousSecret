9b028308d1acfec118e44571812757d160fe98e7
user.name <antheau@yahoo.fr>
2019-03-13 22:56:03 +0100

    envoi partie result + rest

--- Client/client.js
+++ Client/client.js
@@ -3,12 +3,18 @@
   var socket = io.connect('http://localhost:8080');
   // Pages du jeu
   var pages = ['home', 'loading_page', 'predict', 'ranking', 'result','game']
-
+  var prediction = 2;
+  var id=0;
+  var nombre_oui=0;
+  var nombre_votant=0;
+  var pourcentage_oui=0;
+  var pourcentage_prediction =0;
   $('#form').submit(function(event) {
     event.preventDefault();
     // TODO : prevenir si nbjoeurs est null
     socket.emit('newuser', $('#namejoueur').val(), $('#nbjoueur').val())
-    navigateTo('loading_page')
+    navigateTo('loading_page');
+    $('#loading_message').text('En attente des autres joueurs...');
   });
 
   socket.on('newgame', function(){
@@ -23,12 +29,90 @@
   socket.on('waitingothers', function(){
     console.log("waiting");
     navigateTo("loading_page");
+    $('#loading_message').text('En attente des autres joueurs...');
   })
+
+  socket.on('id_chargement', function(id_c){
+  id = id_c;
+})
 
   socket.on('beginningame', function(){
     // TODO
     console.log("beginninggame_client")
     navigateTo('game');
+  })
+
+  socket.on('finVote', function(){
+    navigateTo('result');
+  })
+
+  socket.on('finPredict', function(){
+    navigateTo('result');
+  })
+
+  socket.on('nombre_oui_envoy', function(nb_oui, nb_votant){
+  // TODO
+  nombre_oui=nb_oui;
+  nombre_votant=nb_votant;
+  var vert =0;
+  var rouge = 0;
+  var rouge_int=0;
+  if(prediction>nombre_oui)
+  {
+    vert = nombre_oui/nombre_votant*100;
+    rouge_int=prediction-nombre_oui;
+    rouge = (prediction-nombre_oui)/nombre_votant*100;
+  }
+
+  if(prediction<nombre_oui)
+  {
+    vert =prediction/nombre_votant*100;
+    rouge_int=nombre_oui-prediction;
+    rouge = (nombre_oui-prediction)/nombre_votant*100;
+  }
+  if(prediction==nombre_oui)
+  {
+    vert =prediction/nombre_votant*100;
+    rouge = 0;
+  }
+  console.log("pour="+vert);
+  set_progress(vert,rouge);
+
+
+  navigateTo('result');
+  socket.emit('score_tour', rouge_int,id);
+})
+
+function set_progress(_num,_num2){
+	$('#progress').empty();
+	var el_1_width=_num;
+	var el_2_width=_num2;
+//	var el_3_width=0;
+//	var el_4_width=0;
+//if(_num>30){el_1_width=30;}else{el_1_width=_num;}
+//	if(_num>60){el_2_width=30;}else{el_2_width=_num-el_1_width;}
+//	if(_num>80){el_3_width=30;}else{el_3_width=_num-el_1_width-el_2_width;}
+//	if(_num>90){el_4_width=_num-90;}
+//	var new_font_clor='';
+//	if(_num<55){new_font_clor='color:black';}
+//	$('#progress').append('<div class="progress-text" style="'+new_font_clor+'">'+_num+' %</div>');
+	$('#progress').append('<div class="progress-el" style="background-color:green; width:'+el_1_width+'%;">&nbsp;</div>');
+	$('#progress').append('<div class="progress-el" style="background-color:red; width:'+el_2_width+'%;">&nbsp;</div>');
+//	$('#progress').append('<div class="progress-el" style="background-color:yellow; width:'+el_3_width+'%;">&nbsp;</div>');
+//	$('#progress').append('<div class="progress-el" style="background-color:red; width:'+el_4_width+'%;">&nbsp;</div>');
+
+}
+
+
+
+  socket.on('goranking',function(users){
+  	var list = document.getElementById("todo-list");
+  	for(var i=0;i<users.length;i++){
+  		text = "<li><tr> <th scope=\"row\">"+i+"</th> <td>"+users[i].name+"</td> <td>"+users[i].score+"</td> </tr></li>";
+  		//text = "<li>"+users[i].score+"<input onclick=\"remTache(this)\" type=\"button\" value=\"Supprimer\"></li>";
+  		list.innerHTML+=text;
+  	}
+  	navigateTo('ranking')
   })
 
   // Fonction pour naviguer entre les pages
@@ -50,29 +134,22 @@
     }
   }
 
-  $('#true1').click(function () {
-
-  socket.emit('boolean', 1);
-  navigateTo("loading_page");
-
+  $('#btn_oui').click(function () {
+      socket.emit('vote', true);
+      navigateTo("loading_page");
+      $('#loading_message').text('En attente que les autres répondent...');
   })
 
-  $('#false1').click(function () {
+  $('#btn_non').click(function () {
+    socket.emit('vote', false);
+    navigateTo("loading_page");
+  })
 
-  socket.emit('boolean', 0);
-  navigateTo("loading_page");
-
+  $('#btnPredict').click(function () {
+    prediction = $('#slider').data("roundSlider").getValue();
+    socket.emit('predict');
+    navigateTo("loading_page");
+    $('#loading_message').text(prediction);
   })
 
 })(jQuery);
-
-$("#slider").roundSlider({
-    width: 22,
-    radius: 100,
-    value: 0,
-    lineCap: "round",
-    sliderType: "min-range",
-    startAngle: 90,
-    max: "15",
-    mouseScrollAction: true
-});
--- Serveur/serveur.js
+++ Serveur/serveur.js
@@ -1,21 +1,35 @@
 var PORT = 8080;
-var http = require('http');
+var http = require("http");
 
 httpServer = http.createServer(function(req,res) {
-	console.log('une nouvelle connexion');
-	res.end('Bienvenue');
+	console.log("une nouvelle connexion");
+	res.end("Bienvenue");
 });
 
 httpServer.listen(8080);
 
 
-var io = require('socket.io').listen(httpServer);
+var io = require("socket.io").listen(httpServer);
 
-console.log('Server running on ' + PORT);
+console.log("Server running on " + PORT);
 
 var created = false;
 var users = new Array;
 var nbusers = 0;
+var nbVotants = 0;
+var nbOui = 0;
+var nbPredict = 0;
+var nbTours = 5;
+var questions = [
+	"J’ai déjà volé dans un magasin",
+	"J’ai déjà eu une contravention",
+	"J’ai déjà eu un accident de voiture",
+	"J’ai déjà pris l’avion",
+	"Je me suis déjà fait arrêter par la police",
+	"Je suis déjà resté éveillé plus de 24h d'affilé",
+	"J'ai déjà perdu connaissance",
+	"J'ai déjà pensé que je suis la plus jolie/le plus beau de ce groupe"
+]
 
 // Fonction pour naviguer entre les pages
 function createJoueur(name, score) {
@@ -29,25 +43,25 @@ function createJoueur(name, score) {
 	return joueur;
 }
 
-io.sockets.on('connection',function(socket) {
+io.sockets.on("connection",function(socket) {
+
+	//score du tour
+socket.on('score_tour',function(score,id_client)
+{
+	users[id_client-1].score += score;
+	  console.log("score="+users[id_client-1].score);
+})
 
 	// Faire une tempo pour la page de résultat intermédiaire
-	socket.on('boolean', function (boolean) {
 
-      console.log('Joueur a voter: ' + boolean);
-
-    });
 	if(created && users.length < nbusers){
-		socket.emit('joingame');
+		socket.emit("joingame");
 	}
-	console.log('Nouveau utilisateur');
+	console.log("Nouveau utilisateur");
 
-	socket.on("newuser", function(namejoueur, nbjoueurs){
-		// 3 cas
-		// Cas 1 : premier nbjoueur
-		// Cas 2 : joueur qui rejoint
-		// Cas 3 : joueur en trop
-		console.log('Name : '+namejoueur+'   nbjoueurs : '+nbjoueurs);
+	socket.on("newuser", function(namejoueur, nbjoueurs, mode_jeu){
+
+		console.log("Name : "+namejoueur+"   nbjoueurs : "+nbjoueurs);
 		// First user
 		if(!created && nbjoueurs != null){
 			console.log("premier joueur")
@@ -55,9 +69,11 @@ io.sockets.on('connection',function(socket) {
 
 			// On initialise le score à 0
 			users.push(createJoueur(namejoueur, 0));
-			console.log(users);			socket.emit('waitingothers');
+			console.log(users);
+			socket.emit('waitingothers');
+			socket.emit('id_chargement',users[users.length-1].id);
 			created = true;
-			socket.broadcast.emit('joingame');
+			socket.broadcast.emit("joingame");
 		}
 		else{
 
@@ -65,15 +81,52 @@ io.sockets.on('connection',function(socket) {
 			if(created && users.length < nbusers){
 				console.log("Joining user")
 				users.push(createJoueur(namejoueur, 0));
-				console.log(users);			if(users.length == nbusers){
+				console.log(users);
+				socket.emit('id_chargement',users[users.length-1].id);
+
+				if(users.length == nbusers){
 					console.log("beginninggame")
-					socket.broadcast.emit('beginningame');
+					io.emit("beginningame");
 				}
 				else{
 					console.log("on attend les autres");
-					socket.emit('waitingothers');
+					socket.emit("waitingothers");
 				}
 			}
 		}
 	})
+
+	// Vote du joueur
+	socket.on("vote", function (reponse) {
+		nbVotants += 1;
+		if(reponse) {
+			nbOui += 1;}
+		if(nbVotants == nbusers){
+			io.emit("finVote");
+			io.emit('nombre_oui_envoy',nbOui,nbVotants);
+		}
+	});
+
+	socket.on("predict", function () {
+		nbPredict += 1;
+		if(nbPredict == nbusers){
+			io.emit("finPredict");
+		}
+	});
+
+	socket.on('finPartie', function(){
+		socket.broadcast.emit('goranking',users.sort(function compare(a, b) {
+			  if (a.score < b.score)
+			     return -1;
+			  if (a.score > b.score)
+			     return 1;
+			  // a doit être égal à b
+			  return 0;
+			}));
+		});
+
+
 });
+
+//TODO : réussir a intégrer le code de predict.js dans le client ou le serveur
+//TODO : configurer le nombre max du slider pour être le nombre de joueur
